90765c5ba4735ac20f56256cd9b00c3408674573
Rajesh G <rajeshg3@outlook.com>
2023-11-04 15:44:33 +0530

    feat: view patient

--- main.py
+++ main.py
@@ -20,29 +20,41 @@ while(1):
 
     main_menu_choice = int(input())
     if main_menu_choice == 1:
-                print('What would you like to do?')
-                print('1. Add patient')
-                print('2. Update patient')
-                print('3. Delete patient')
-                print('4. View patient details')
-                print('5. Go to previous menu')
-                print('\nEnter your choice:')
-        pat_menu_choice = int(input())
-        if pat_menu_choice == 1:
-            print('\nEnter patient name:')
-            pat_name = str(input())
-            print('\nEnter addressLine1:')
-            pat_addr1 = str(input())
-            print('\nEnter addressLine2:')
-            pat_addr2 = str(input())
-            print('\nEnter city:')
-            pat_city = str(input())
-            print('\nEnter phone number:')
-            pat_phone = str(input())
-            cursor.execute('''INSERT INTO patients(name,addressline1,addressline2,city,phone_number)
-                                VALUES(?,?,?,?,?)''', (pat_name,pat_addr1,pat_addr2,pat_city,pat_phone))
-            db.commit()
-            print("patient successfully added!\n")
+        while(1):
+            print(' What would you like to do?')
+            print(' 1. Add patient')
+            print(' 2. Update patient')
+            print(' 3. Delete patient')
+            print(' 4. View patient details')
+            print(' 5. Go to previous menu')
+            print(' \nEnter your choice:')
+
+            pat_menu_choice = int(input())
+            if pat_menu_choice == 1:
+                print('\n   Enter patient name:')
+                pat_name = str(input())
+                print('\n   Enter address:')
+                pat_addr = str(input())
+                print('\n   Enter phone number:')
+                pat_phone = str(input())
+                cursor.execute('''INSERT INTO 
+                                        patients(name,address,phone_number)
+                                    VALUES(?,?,?)''', (pat_name,pat_addr,pat_phone))
+                db.commit()
+                print("patient successfully added!\n")
+            elif pat_menu_choice == 4:
+                print('\n   The patients are:\n')
+                cursor.execute('''SELECT * FROM patients''')
+                print('\tid\tname\taddress\t\tphone_number')
+                for row in cursor:
+                    print(f'\t{row[0]}\t{row[1]}\t{row[2]}\t{row[3]}')
+                print('\n\n')
+            elif pat_menu_choice == 5:
+                break
+            else:
+                print('Enter a valid choice!\n')
+    elif main_menu_choice == 7:
+        break
     else:
         print('Enter a valid choice!\n')
 
